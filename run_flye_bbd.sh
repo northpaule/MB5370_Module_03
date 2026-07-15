5b7456b810130eaf44fc08d4d6b60a570837fc90
northpaule <pmathieu003@gmail.com>
2026-07-15 00:50:37 +0000

    organization

--- run_flye_bbd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=360
-#SBATCH --ntasks=12 --mem=10gb
-
-echo "Starting flye bbd in $(pwd) at $(date)"
-
-flye --nano-hq ../raw_reads/bbd_0.1.fastq.gz --threads 12 --out-dir bbd --meta
-
-echo "Finished flye bbd in $(pwd) at $(date)"
